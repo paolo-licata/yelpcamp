8bada5d5e4b9a5b3d5b683856350e0617a58cff2
Paolo Licata <paolo_licata@outlook.com>
2024-12-05 11:43:13 +0100

    Added a post endpoint for the creation of a new campground through a simple form ( new.ejs )

--- app.js
+++ app.js
@@ -16,16 +16,31 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
 	res.render('home')
 })
 
+//Gets a list of all campgrounds
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
 	console.log(campgrounds);
 	res.render('campgrounds/index', { campgrounds })
 })
 
+//Gets the form for new campground
+app.get('/campgrounds/new', async (req, res) => {
+	res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+	const campground = new Campground(req.body.campground);
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`)
+})
+
+//Gets a single campground by ID
 app.get('/campgrounds/:id', async (req, res) => {
 	const campground = await Campground.findById(req.params.id)
 	res.render('campgrounds/show', { campground })
