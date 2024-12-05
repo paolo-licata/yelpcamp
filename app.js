fed64eb9be83259fa5fd93cc785bb9fa4001045b
Paolo Licata <paolo_licata@outlook.com>
2024-12-05 12:30:51 +0100

    Closing CRUD with implementation of a delete endpoint and button on show.ejs

--- app.js
+++ app.js
@@ -61,6 +61,13 @@ app.put('/campgrounds/:id', async (req, res) => {
 	res.redirect(`/campgrounds/${campground._id}`)
 })
 
+//Delete a campground by ID
+app.delete('/campgrounds/:id', async (req, res) => {
+	const { id } = req.params;
+	await Campground.findByIdAndDelete(id);
+	res.redirect('/campgrounds');
+})
+
 app.listen(3000, () => {
 	console.log('Serving on port 3000.')
 })
