8c8500d710072dae616fc668e5dee1060896fe83
Paolo Licata <paolo_licata@outlook.com>
2024-12-08 19:14:19 +0100

    Created a simple Navbar and footer with Bootstrap v5 and ejs-mate

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override');
 const Campground = require('./models/campground')
 
@@ -14,6 +15,7 @@ db.once('open', () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -27,7 +29,6 @@ app.get('/', (req, res) => {
 //Gets a list of all campgrounds
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
-	console.log(campgrounds);
 	res.render('campgrounds/index', { campgrounds })
 })
 
